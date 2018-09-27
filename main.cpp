b8cdbeec12afdd6f4dfd6605fb51b71bd3103fde
vinnytmv <valopez@mail.csuchico.edu>
2018-09-26 19:03:12 -0700

    Enables user to create gorcerie list

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Vincent Lopez
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,29 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string item;
+do
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+if(input == 'a'|| input == 'A')
+{
+  cout<<"What is the item?\n";
+  cin>>item;
+  if(numItems < 5)
+  {
+    list[numItems] = item;
+    numItems ++;
+  }
+  else
+  { 
+    cout<<"You'll need a bigger list!\n";
+  }
+}
+} while (input!='q' && input!='Q');
   return 0;
 }
